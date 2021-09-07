c65114b133510b4ba94799a3e07a28346cd3d73d
chinoyeh <ruthochons@gmail.com>
2021-09-07 17:50:00 +0100

    third

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import Navbar from './Navbar'
 import Footer from './Footer'
-import Cole from './images/Me.JPG'
+// import Cole from './images/Me.JPG'
 
 
 
@@ -13,7 +13,7 @@ const Home = () => {
         <div className ="homepage">
             <Navbar/>
       <div className ="home">
-      <div className= 'home-image'> <img alt ="Me"src= {Cole}/></div>
+      <div className= 'home-image'> <img alt ="Me"src= '#'/></div>
 
 <article>
     <p> I'm Chinoyeh, a react developer, writer and part-time make-up artist. I studued Computer Science from the pretigious Bowen University Iwo</p>
